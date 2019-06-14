cf1a6695dceff3ad313f9908d3a6f3af47eb8d5f
Andrey <megokryak@yandex.ru>
2019-06-14 15:44:12 +0300

    выполнил настройку гранта

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,54 @@
 // Gruntfile.js
- 
+
 // Вся конфигурация находится внутри этой функции
 module.exports = function(grunt) {
- 
+
   // ===========================================================================
   // Конфигурация GRUNT ===========================================================
   // ===========================================================================
-  grunt.initConfig({
- 
-   //Здесь будут указаны модули и их настройки
- 
-  });
- 
+
   // ===========================================================================
   // Загружаем модули GRUNT ========================================================
   // ===========================================================================
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-postcss');
   grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
- 
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.initConfig({
+
+   //Здесь будут указаны модули и их настройки
+   less: {
+    style: {
+      files: {
+        "source/css/style.css": "source/less/style.less"
+      }
+    }
+   },
+   postcss: {
+    style: {
+      options: {
+        processors: [
+          require("autoprefixer")()
+        ]
+      },
+      src: "source/css/*.css"
+    }
+   },
+   watch: {
+    style: {
+      files: ["source/less/**/*.less"],
+      tasks: ["less", "postcss"]
+    }
+   },
+   browserSync: {
+    server: {
+      bsFiles: {
+        src: ["source/*.html", "source/css/*.css"]
+      },
+      options: {
+        server: "sorce/"
+      }
+    }
+   }
+  });
 };
